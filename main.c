08f100d9af503ef39f504ebc3946f47ca130ccf7
Oldich Pulkrt <O.Pulkrt@gmail.com>
2013-01-04 23:03:42 +0100

    Many new changes (see commit description)
    Added new constants
    Added new attributes to structure GameState
    Added new function getRowsCount (return number of rows)
    Added new function printMatrxi (prints 2D matrix to screen)
    Added function getManhattanDistance (sums manhattan distances, will be
    used as heuristic function)
    In main method are these function used

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INITIAL_BOARD_SIZE 1
-#define NO_PARAMETER -2
+#define POSITION_ZERO 16
+#define MISSING_PARAMETER 1
+#define MALFORMED_INPUT 2
 
 
 /***************** DATA STRUCTURES ***************************/
 typedef struct 
 {
-        int tilesPosition[INITIAL_BOARD_SIZE][INITIAL_BOARD_SIZE];
+        int** tilesPosition;
+        int manhattanDistance;
+        int distance;
 } GameState;
 
 /**************** FUNCTION PROTOTYPES ********************/
@@ -16,6 +19,9 @@ int canMoveLeft(GameState* thisState);
 int canMoveRight(GameState* thisState);
 int canMoveUp(GameState* thisState);
 int canMoveDown(GameState* thisState);
+int getRowsCount(char* initState);
+void printMatrix(int** matrix);
+int getManhattanDistance(GameState state);
 
 
 /********************** FUNCTION DEFINITIONS *****************/
@@ -55,37 +61,158 @@ int canMoveDown(GameState* thisState)
     return 1;
 }
 
+/**
+    Returns number of rows in given string (according to format of parameter)
+*/
+int getRowsCount(char* initState)
+{
+    int rows = 0;
+    int length = strlen(initState);
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        if (initState[i] == ';')
+        {
+            // we don't count semicolon on last position of parameter
+            if (i != strlen(initState) - 1)
+            {
+                rows++;    
+            }
+        }    
+    }
+    
+    return rows;
+}
 
-/********************** MAIN PROGRAM********************/
+/**
+    Prints two dimesnional array
+*/
+void printMatrix(int** matrix)
+{
+    int length = sizeof(matrix);
+    int i, j;
+    for (i = 0; i < length; i++)
+    {
+        for (j = 0; j < length; j++)
+        {
+            printf("%d ", matrix[i][j]);    
+        }    
+        printf("\n");
+    }
+}
+
+/**
+    Returns sum of manhattan distances between number and their right position
+*/
+int getManhattanDistance(GameState state)
+{
+    int manhattan = 0;
+    int number = 0;
+    int row;
+    int i, j, k, l;
+    int length = sizeof(state.tilesPosition); 
+    for (i = 0; i < length; i++)
+    {
+        for (j = 0; j < length; j++)
+        {
+            number = state.tilesPosition[i][j];
+            if (number == 0)
+            {
+                number = POSITION_ZERO;
+            }
+            row = (number - 1) / length;
+            manhattan += abs(row - i);
+            manhattan += abs((number - 1) % length - j);
+        }
+    }
+    
+    return manhattan;
+}
+
+
+
+/********************** MAIN PROGRAM ********************/
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-             printf("Nebyl zadan parametr.\n");
-             return NO_PARAMETER;
+             puts("ERR#1: Missing argument!");
+             return MISSING_PARAMETER;
     }
-    
+        
     // copy argv[1] to variable initState
     char initState[strlen(argv[1])];
     strcpy(initState, argv[1]);
     
+    int i, j;
+    int length = strlen(initState);
+    GameState state;
+    state.distance = 0;
+    GameState priorityQueue[30];
     
-    int rows = 0;
-    int cols = 0;
-    char * pch;
-    printf ("Splitting string \"%s\" into tokens:\n", initState);
-    pch = strtok(initState,";");
-    while (pch != NULL)
+    // count rows
+    int rows = getRowsCount(initState);
+    
+    // there is one less semicolon than rows
+    rows++;
+    
+    // allocation of memory for initial state of game
+    state.tilesPosition = (int *) malloc(rows * sizeof(int *));
+    for (i = 0; i < rows; i++)
     {
-          printf ("%s\n",pch);
-          pch = strtok(NULL, ";");
+        state.tilesPosition[i] = malloc(rows * sizeof(int *));   
+        for (j = 0; j < sizeof(state.tilesPosition[i]); j++)
+        {
+            state.tilesPosition[i][j] = 0;    
+        } 
     }
     
+    rows = 0;
+    int cols = 0;
+    int prevCols = 0;
+    int number = 0;
+    for (i = 0; i < length; i++)
+    {
+        if (initState[i] == ';')
+        {
+            // if count of columns doesn't match, input format isn't right
+            if (cols != prevCols && prevCols != 0)
+            {
+                printf("ERR#2: Malformed input!");
+                system("PAUSE");
+                return MALFORMED_INPUT;
+            }
+            state.tilesPosition[rows][cols] = number;
+            
+            // init for next row
+            rows++;
+            prevCols = cols;
+            cols = 0;
+            number = 0;
+            
+            // if next character after semicolon is space, jumo after it
+            if (initState[i + 1] == ' ')
+            {
+                i += 2;    
+            }
+        }
+        if (initState[i] == ' ')
+        {
+            state.tilesPosition[rows][cols] = number;
+            cols++;    
+            number = 0;
+        }
+        if (isdigit(initState[i]))
+        {
+            number = number * 10 + (initState[i] - 48);
+        }
+    }
+    
+    printMatrix(state.tilesPosition);
     
     
-    printf("%s\n", initState);
-    
+    printf("%d\n", getManhattanDistance(state));
     system("PAUSE");
     return 0;
 }
